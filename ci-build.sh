e06386b0e42d5d64ee5d1e807e75dcb29d22f930
Jaryl Chng <git@jarylchng.com>
2021-06-26 04:07:31 +0800

    make ci-build.sh more universal

--- ci-build.sh
+++ ci-build.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
-GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o carousell-gobot.windows-amd64.exe
-GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o carousell-gobot.linux-amd64
-GOOS=linux GOARCH=arm64 go build -ldflags="-w -s" -o carousell-gobot.linux-arm64
-GOOS=linux GOARCH=arm go build -ldflags="-w -s" -o carousell-gobot.linux-arm-v7
+GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o "$(basename "${PWD}").windows-amd64.exe"
+GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o "$(basename "${PWD}").linux-amd64"
+GOOS=linux GOARCH=arm64 go build -ldflags="-w -s" -o "$(basename "${PWD}").linux-arm64"
+GOOS=linux GOARCH=arm go build -ldflags="-w -s" -o "$(basename "${PWD}").linux-arm-v7"
